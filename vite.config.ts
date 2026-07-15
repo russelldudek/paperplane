6125c225c7002151faa6bceb44b2e86503499be8
russelldudek <35631821+russelldudek@users.noreply.github.com>
2026-07-15 07:47:09 -0400

    fix: build a broadly compatible production bundle

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/paperplane/',
   plugins: [react()],
+  build: {
+    target: 'es2019',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
